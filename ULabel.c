b655a65ff7915b0c55a214b3a23b7f0fa1a39f44
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-07-06 00:04:41 -0700

    Drop CRAY/WORD64 support (unifdef -UCRAY -UWORD64)
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- ULabel.c
+++ ULabel.c
@@ -67,9 +67,6 @@ SOFTWARE.
 
 #define MULTI_LINE_LABEL 32767
 
-#ifdef CRAY
-#define WORD64
-#endif
 
 /****************************************************************
  *
@@ -179,51 +176,9 @@ static void ClassInitialize(void)
 static XChar2b *buf2b;
 static int buf2blen = 0;
 
-#ifndef WORD64
 
 #define TXT16 XChar2b
 
-#else
-
-#define TXT16 char
-
-static int _XawLabelWidth16(XFontStruct *fs, char *str, int n)
-{
-    int i;
-    XChar2b *ptr;
-
-    if (n > buf2blen) {
-	buf2b = (XChar2b *)XtRealloc((char *)buf2b, n * sizeof(XChar2b));
-	buf2blen = n;
-    }
-    for (ptr = buf2b, i = n; --i >= 0; ptr++) {
-	ptr->byte1 = *str++;
-	ptr->byte2 = *str++;
-    }
-    return XTextWidth16(fs, buf2b, n);
-}
-
-static void _XawLabelDraw16(Display *dpy, Drawable d, GC gc,
-			    int x, int y, char *str, int n)
-{
-    int i;
-    XChar2b *ptr;
-
-    if (n > buf2blen) {
-	buf2b = (XChar2b *)XtRealloc((char *)buf2b, n * sizeof(XChar2b));
-	buf2blen = n;
-    }
-    for (ptr = buf2b, i = n; --i >= 0; ptr++) {
-	ptr->byte1 = *str++;
-	ptr->byte2 = *str++;
-    }
-    XDrawString16(dpy, d, gc, x, y, buf2b, n);
-}
-
-#define XTextWidth16 _XawLabelWidth16
-#define XDrawString16 _XawLabelDraw16
-
-#endif /* WORD64 */
 
 static void _XawLabelDrawUCS(Display *dpy, Drawable d, GC gc,
 			     int x, int y, char *str, int n)
